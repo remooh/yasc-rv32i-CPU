aaddc9574cafa15a4706b16c8f27743892260c8f
Remo Held <remoheld@gmail.com>
2020-09-10 01:44:56 -0300

    added comments

--- register_file.v
+++ register_file.v
@@ -1,12 +1,14 @@
 module register_file
 #(      parameter DATA_WIDTH = 32)
 (
+	// Input Ports
 	input clk,
 	input	rd_we,
 	input [4:0] rd_addr,
 	input [4:0] rs1_addr,
 	input [4:0] rs2_addr,
 	input [DATA_WIDTH-1:0] rd_data,
+	// Output Ports
 	output reg [DATA_WIDTH-1:0] rs1_data,
 	output reg [DATA_WIDTH-1:0] rs2_data
 );
