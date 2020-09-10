cf49d0b893739d4d08b910071c295a5dbc72e3c8
Remo Held <remoheld@gmail.com>
2020-09-10 01:45:25 -0300

    module instantiation done

--- rv32i_cpu.v
+++ rv32i_cpu.v
@@ -11,7 +11,7 @@ module rv32i_cpu
 	
 	// instruction memory
 	output [IMEM_WIDTH-1:0] inst_addr,
-	input  [31:0] instruction,
+	input  [31:0] instruction_data,
 	
 	// data memory
 	output [DMEM_WIDTH-1:0] data_addr,
@@ -20,9 +20,109 @@ module rv32i_cpu
 	input  [31:0] data_read
 
 );
+//////////////////// wires and registers ////////////////////
+	// register storing instruction information
+	reg  [31:0] instruction; 
+
+	// decode fields
+	wire [6:0] opcode;
+	wire [4:0] rd_addr;
+	wire [2:0] funct3;
+	wire [4:0] rs1_addr;
+	wire [4:0] rs2_addr;
+	wire [6:0] funct7;
+	wire [31:0] immediate;
+
+	// control unit fields
+	wire [2:0] alu_src;
+	wire [3:0] alu_op;
+	wire [2:0] jump_type;
+	wire [1:0] regfile_src;
+	wire [2:0] mem_read_type;
+	wire [3:0] mem_write_mask;
+	
+	// program counter fields
+	wire update_pc;
+	wire [31:0] pc_plus_4;
+	wire [31:0] pc_next;
+	
+	// register file fields
+	wire rd_we;
+	wire [31:0] rd_data;
+	wire [31:0] rs1_data;
+	wire [31:0] rs2_data;
+
+	// alu fields
+	wire [31:0] alu_a;
+	wire [31:0] alu_b;
+	wire [31:0] alu_result;
+
+
+//////////////////// instantiate modules ////////////////////
+	decode _decode(
+		//inputs
+		.instruction (instruction),
+		//outputs
+		.opcode (opcode),
+		.rd_addr (rd_addr),
+		.funct3 (funct3),
+		.rs1_addr (rs1_addr),
+		.rs2_addr (rs2_addr),
+		.funct7 (funct7),
+		.inst_type (inst_type),
+		.immediate (immediate)
+	);
+	
+	control_unit _control(
+		//inputs
+		.opcode (opcode),
+		.funct3 (funct3),
+		.funct7 (funct7),
+		//outputs
+		.alu_src (alu_src),
+		.alu_op (alu_op),
+		.jump_type (jump_type),
+		.regfile_src (regfile_src),
+		.mem_read_type (mem_read_type),
+		.mem_write_mask (mem_write_mask)
+	);
+	
+	program_counter _pc(
+		//inputs
+		.addr_offset (immediate),
+		.alu_result (alu_result),
+		.jump_type (jump_type),
+		.update_pc (update_pc),
+		//outputs
+		.pc_plus_4 (pc_plus_4),
+		.pc_next (pc_next)
+	);
+	
+	register_file _regfile(
+		//inputs
+		.clk (clk),
+		.rd_we (rd_we), 
+		.rd_addr (rd_addr),
+		.rs1_addr (rs1_addr),
+		.rs2_addr (rs2_addr),
+		.rd_data (rd_data),
+		//outputs
+		.rs1_data (rs1_data),
+		.rs2_data (rs2_data)
+	);
+	
+	alu _alu(
+		//inputs
+		.alu_op (alu_op),
+		.operand_a (alu_a),
+		.operand_b (alu_b),
+		//outputs
+		.result (alu_result)
+	);
+	
 
 	// cpu finite state machine
-	localparam IDLE             = 0;
+	localparam IDLE	= 0;
 	
 	reg [2:0] current_state, next_state;
 	
