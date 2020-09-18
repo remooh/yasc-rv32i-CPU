f45237534fa16b86cae9d1feb354c25a31469e57
Remo Held <remoheld@gmail.com>
2020-09-18 01:21:24 -0300

    partial riscv-formal interface implementation
    Signed-off-by: Remo Held <remoheld@gmail.com>

--- rv32i_cpu.v
+++ rv32i_cpu.v
@@ -10,15 +10,53 @@ module rv32i_cpu
 	input reset,
 	
 	// instruction memory
-	output reg [IMEM_WIDTH-1:0] inst_addr,
-	input  [31:0] instruction_data,
+	output reg [IMEM_WIDTH-1:0] 	inst_addr,
+	input  [31:0] 			instruction_data,
 	
 	// data memory
-	output reg [DMEM_WIDTH-1:0] data_addr,
-	output reg [3:0] data_we,
-	output reg [31:0] data_write,
-	input [31:0] data_read,
-	input data_valid
+	output reg [DMEM_WIDTH-1:0] 	data_mem_addr,
+	output reg [3:0] 		data_mem_wmask,
+	output reg [31:0] 		data_mem_write,
+	input [31:0]			data_mem_read,
+	input 				data_mem_valid
+
+`ifdef RISCV_FORMAL
+	localparam XLEN = 32;	// register file width
+	localparam NRET = 1;	// max number of retired instructions per cycle
+	localparam ILEN = 32;	// max instruction width
+
+	// Instruction Metadata
+	output reg [NRET        - 1 : 0] rvfi_valid,
+	output reg [NRET *   64 - 1 : 0] rvfi_order,
+	output reg [NRET * ILEN - 1 : 0] rvfi_insn,
+	output reg [NRET        - 1 : 0] rvfi_trap,
+	output reg [NRET        - 1 : 0] rvfi_halt,
+	output reg [NRET        - 1 : 0] rvfi_intr,
+	output reg [NRET * 2    - 1 : 0] rvfi_mode,
+	output reg [NRET * 2    - 1 : 0] rvfi_ixl,
+
+	// Integer Register Read/Write
+	output reg [NRET *    5 - 1 : 0] rvfi_rs1_addr,
+	output reg [NRET *    5 - 1 : 0] rvfi_rs2_addr,
+	output reg [NRET * XLEN - 1 : 0] rvfi_rs1_rdata,
+	output reg [NRET * XLEN - 1 : 0] rvfi_rs2_rdata,
+	output reg [NRET *    5 - 1 : 0] rvfi_rs1_addr,
+	output reg [NRET *    5 - 1 : 0] rvfi_rs2_addr,
+	output reg [NRET * XLEN - 1 : 0] rvfi_rs1_rdata,
+	output reg [NRET * XLEN - 1 : 0] rvfi_rs2_rdata,
+
+	// Program Counter
+	output reg [NRET * XLEN - 1 : 0] rvfi_pc_rdata,
+	output reg [NRET * XLEN - 1 : 0] rvfi_pc_wdata,
+
+	// Memory Access
+	output reg [NRET * XLEN   - 1 : 0] rvfi_mem_addr,
+	output reg [NRET * XLEN/8 - 1 : 0] rvfi_mem_rmask,
+	output reg [NRET * XLEN/8 - 1 : 0] rvfi_mem_wmask,
+	output reg [NRET * XLEN   - 1 : 0] rvfi_mem_rdata,
+	output reg [NRET * XLEN   - 1 : 0] rvfi_mem_wdata,
+`endif
+
 
 );
 //////////////////// wires and registers ////////////////////
@@ -42,7 +80,7 @@ module rv32i_cpu
 	wire [1:0] regfile_src;
 	wire [1:0] mem_op;
 	wire [2:0] mem_read_type;
-//	wire [3:0] mem_write_mask;
+	wire [3:0] mem_write_mask;
 	
 	// program counter fields
 	reg update_pc;
@@ -63,9 +101,12 @@ module rv32i_cpu
 	wire [31:0] alu_result;
 
 	// sign extend read data
-	wire data_sign;
-	wire [31:0] data_signed;
+	wire data_mem_sign;
+	wire [31:0] data_mem_signed;
 
+`ifdef RISCV_FORMAL
+	wire [3:0] get_read_mask;
+`endif
 
 //////////////////// instantiate modules ////////////////////
 	decode _decode(
@@ -135,21 +176,21 @@ module rv32i_cpu
 //////////////////// internal wiring ////////////////////
 	
 	// sign extend input data
-	assign data_sign = 	(mem_read_type == `MEM_RD_BYTE) ? data_read[7]:
-				(mem_read_type == `MEM_RD_HALF) ? data_read[7]:
+	assign data_mem_sign = 	(mem_read_type == `MEM_RD_BYTE) ? data_mem_read[7]:
+				(mem_read_type == `MEM_RD_HALF) ? data_mem_read[7]:
 				1'b0;
 
-	assign data_signed =	(mem_read_type == `MEM_RD_BYTE)	? {{24{data_sign}}, data_read[7:0]}:	// lb
-				(mem_read_type == `MEM_RD_HALF)	? {{16{data_sign}}, data_read[15:0]}:	// lh
-				(mem_read_type == `MEM_RD_B_U) 	? {{24{1'b0}}, data_read[7:0]}:		// lbu
-				(mem_read_type == `MEM_RD_H_U) 	? {{16{1'b0}}, data_read[15:0]}:	// lhu
-				data_read;								// lw
+	assign data_mem_signed =(mem_read_type == `MEM_RD_BYTE)	? {{24{data_mem_sign}}, data_mem_read[7:0]}:	// lb
+				(mem_read_type == `MEM_RD_HALF)	? {{16{data_mem_sign}}, data_mem_read[15:0]}:	// lh
+				(mem_read_type == `MEM_RD_B_U) 	? {{24{1'b0}}, data_mem_read[7:0]}:		// lbu
+				(mem_read_type == `MEM_RD_H_U) 	? {{16{1'b0}}, data_mem_read[15:0]}:		// lhu
+				data_mem_read;									// lw
 
 	// register file
 	assign regfile_addr =	(regfile_src != `REG_SRC_NONE) 	? rd_addr : {5{1'b0}};
 
 	assign rd_data = 	(regfile_src == `REG_SRC_ALU) 	? alu_result:
-				(regfile_src == `REG_SRC_MEM) 	? data_signed:
+				(regfile_src == `REG_SRC_MEM) 	? data_mem_signed:
 				(regfile_src == `REG_SRC_IMM) 	? immediate:
 				(regfile_src == `REG_SRC_PCP4) 	? pc_plus_4:
 				{32{1'b0}};
@@ -160,6 +201,13 @@ module rv32i_cpu
 	// alu 2nd operand
 	assign alu_b = 	(rs2_to_alu2 == `ALU2_SRC_RS2) ? rs2_data : immediate;
 
+`ifdef RISCV_FORMAL
+	assign get_read_mask =	(mem_read_type == `MEM_RD_BYTE)	? 4'b0001:
+				(mem_read_type == `MEM_RD_HALF)	? 4'b0011:
+				(mem_read_type == `MEM_RD_B_U) 	? 4'b0001:
+				(mem_read_type == `MEM_RD_H_U) 	? 4'b0011:
+				4'b1111;
+`endif
 
 //////////////////// cpu finite state machine ////////////////////
 	localparam IDLE	= 0;
@@ -176,6 +224,11 @@ module rv32i_cpu
 		
 		case(current_state)
 			IDLE: begin
+				next_state = FETCH;
+`ifdef RISCV_FORMAL
+				rvfi_valid = 1'b0;
+				rvfi_order = {NRET*64{1'b0}};
+`endif
 			end
 
 			FETCH: begin
@@ -188,6 +241,10 @@ module rv32i_cpu
 				
 				// fetch instruction
 				instruction = instruction_data;
+`ifdef RISCV_FORMAL
+				rvfi_valid = 1'b0;
+				rvfi_order = rvfi_order + (NRET*64)'h1;
+`endif
 			end
 
 			DECODE: begin
@@ -203,7 +260,7 @@ module rv32i_cpu
 				next_state = WRITE_BACK;
 
 				if(mem_op != `MEM_OP_NONE) begin
-					data_addr = alu_result;
+					data_mem_addr = alu_result;
 				end
 
 				if(mem_op == `MEM_OP_LOAD) begin
@@ -211,8 +268,8 @@ module rv32i_cpu
 				end
 				else if(mem_op == `MEM_OP_STORE) begin
 					next_state = WRITE_BACK;
-					data_we = mem_write_mask;
-					data_write = rs2_data;
+					data_mem_wmask = mem_write_mask;
+					data_mem_write = rs2_data;
 				end
 
 				// update program counter
@@ -223,7 +280,7 @@ module rv32i_cpu
 
 			MEMORY_WAIT: begin
 				next_state = MEMORY_WAIT;
-				if(data_valid)
+				if(data_mem_valid)
 					next_state = WRITE_BACK;
 
 				// update program counter
@@ -241,6 +298,30 @@ module rv32i_cpu
 				update_pc = 1'b0;
 				// regfile write enable
 				rd_we = 1'b1;
+`ifdef RISCV_FORMAL
+				// Instruction Metadata
+				rvfi_valid = 1'b1;
+				rvfi_insn = instruction;
+
+				// Integer Register Read/Write
+				rvfi_rs1_addr = rs1_addr;
+				rvfi_rs2_addr = rs2_addr;
+				rvfi_rs1_rdata = rs1_data;
+				rvfi_rs2_rdata = rs2_data;
+				rvfi_rd_addr = rd_addr;
+				rvfi_rd_wdata = rd_data;
+
+				// Program Counter
+				rvfi_pc_rdata = pc_current;
+				rvfi_pc_wdata = inst_addr;
+
+				// Memory Access
+				rvfi_mem_addr = data_mem_addr;
+				rvfi_mem_rmask = get_read_mask;
+				rvfi_mem_wmask = data_mem_wmask;
+				rvfi_mem_rdata = data_mem_read;
+				rvfi_mem_wdata = data_mem_write;
+`endif
 			end
 
 		endcase
