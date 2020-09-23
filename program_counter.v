87d6b9dc3bce72e2ba16f9c7522bb9ac1bc9aaeb
Remo Held <remoheld@gmail.com>
2020-09-23 01:11:14 -0300

    fix syntax error?????
    Signed-off-by: Remo Held <remoheld@gmail.com>

--- program_counter.v
+++ program_counter.v
@@ -23,7 +23,7 @@ module program_counter
 	wire [DATA_WIDTH-1:0] pc_offset;
 	wire [DATA_WIDTH-1:0] jalr_addr;
 	
-	assign pc_plus_4 = program_counter + DATA_WIDTH'h4;
+	assign pc_plus_4 = program_counter + 32'h4;
 	assign pc_offset = program_counter + addr_offset;
 	assign jalr_addr = alu_result & {DATA_WIDTH-1'b1, 1'b0}; // alu result with lsb set to zero
 	
@@ -31,7 +31,7 @@ module program_counter
 				(jump_type == `JUMP_IF_1 && alu_result[0] == 1'b1) ? pc_offset:
 				(jump_type == `JUMP_JAL) ? pc_offset:
 				(jump_type == `JUMP_JALR) ? jalr_addr:
-				(jump_type == `JUMP_ZERO) ? DATA_WIDTH'b0:
+				(jump_type == `JUMP_ZERO) ? {DATA_WIDTH{1'b0}}:
 				pc_plus_4;
 	
 	always @ (posedge update_pc) begin
