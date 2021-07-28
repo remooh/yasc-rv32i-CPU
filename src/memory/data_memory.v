7625a6cb96e1ac56f41d704cf036a72f72305660
Remo Held <remoheld@gmail.com>
2021-07-28 01:48:13 -0300

    Update data memory to properly support the write mask
    Data memory was ignoring the write mask, this commit fixes this.

--- src/memory/data_memory.v
+++ src/memory/data_memory.v
@@ -8,10 +8,7 @@ module data_memory
 	input [31:0] data_write,
 	output [31:0] data_read
 );
-	// Variable to hold the registered read address
-	reg [ADDR_WIDTH-1:0] data_addr_reg;
-
-	//  RAM array
+	// RAM array
 	reg [31:0] ram[2**ADDR_WIDTH-1:0];
 
 	//initial
@@ -21,10 +18,12 @@ module data_memory
 	//		ram[i] = 32'b0;
 	//end
 
+	// initialize memory with the contents in dmem.hex
 	initial begin
 		$readmemh("../memory/dmem.hex", ram);
 	end
 
+	// workaround to dump the memory contents in the testbench VCD
 	generate
 		genvar idx;
 		for(idx = 0; idx < 128; idx = idx+1) begin: dmem_dump
@@ -32,16 +31,25 @@ module data_memory
 			assign tmp = ram[idx];
 		end
 	endgenerate
+
+	wire [7:0] mask0;
+	wire [7:0] mask1;
+	wire [7:0] mask2;
+	wire [7:0] mask3;
+
+	assign mask0 = (mem_wmask[0] == 1'b1) ? data_write[7:0]   : data_read[7:0];
+	assign mask1 = (mem_wmask[1] == 1'b1) ? data_write[15:8]  : data_read[15:8];
+	assign mask2 = (mem_wmask[2] == 1'b1) ? data_write[23:16] : data_read[23:16];
+	assign mask3 = (mem_wmask[3] == 1'b1) ? data_write[31:24] : data_read[31:24];
 	
 	always @ (posedge clk)
 	begin
 		// Write
 		if (we)
-			ram[data_addr] <= data_write;
+			ram[data_addr] <= {mask3, mask2, mask1, mask0};
 
-		data_addr_reg <= data_addr;
 	end
 	
-	assign data_read = ram[data_addr_reg];
+	assign data_read = ram[data_addr];
 
 endmodule
